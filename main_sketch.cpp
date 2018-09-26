f40ad97d5f2a9f1c880e61df2990894192006a18
igorgrebenev <igor.ge@mail.ru>
2018-09-26 10:22:45 +0700

    Update main_sketch.cpp
    Проверено
    https://create.arduino.cc/editor/igorgrebenev/dc8b4924-3321-4df2-8f9f-5545e374162b

--- main_sketch.cpp
+++ main_sketch.cpp
@@ -4,65 +4,74 @@
 const int INITIAL_SERVO_DELAY = 50;
 const int UNDEFINED_PORT = -1;
 
-const int DPORT1 = 1;
 /*
 * Обертка для работы с серво машинкой
 * Mg995 - сервомашинки для манипулятора ROT3U-6DOF
 * https://www.electronicoscaldas.com/datasheet/MG995_Tower-Pro.pdf
 */
-class rot3u_6dof_servo {
-  private int servoPosition = 0;
-  private Servo servo;
-  private int portNumber = UNDEFINED_PORT;
+class SGServo {
+  private:
+  int servoPosition = 0;
+  Servo servo;
+  int portNumber = UNDEFINED_PORT;
   // задержка при повороте на 1 градус
   // каждый сервопривод может иметь свою скорость вращения
-  private int degDelay = INITIAL_SERVO_DELAY;
-  private int getDegDelay() {
+  int degDelay = INITIAL_SERVO_DELAY;
+  int getDegDelay() {
     return degDelay;
   }
-  private void setDegDelay(int value) {
+  void setDegDelay(int value) {
     degDelay = value;
   }
-  __declspec(property(get = getDegDelay, put = setDegDelay)) int DelayPerGrad; 
-  
-  public int getPosition() {
+  int getPortNumber() {
+    return portNumber;
+  }
+
+  public:
+
+  SGServo () {
+    servoPosition = 0;
+  }
+
+  int getPosition() {
     return servoPosition;
   }
 
-  public bool isInitialized() {
+  bool isInitialized() {
     return portNumber != UNDEFINED_PORT;
   }
   
   // Инициализация серво привода
-  public void Initialize(int port) {
+  void initialize(int port) {
     if (isInitialized()) {
       return; // сервопривод уже инициализирован
     }
     portNumber = port; // сохраняем назначенный порт для проверки 
     servo.attach(portNumber); // поключить сервопривод к порту
   }
-  
-  private int getPortNumber() {
-    return portNumber;
-  }
-  // обертка над инициализацией
-  __declspec(property(get = getPortNumber, put = Initialize)) int Port; 
-  
+
   // исполнение сервопривода
   void perform (int to, int delay) {
     int direction = (to - getPosition()) > 0 ? 1 : -1;
     while (to != servoPosition) {
       servo.write(servoPosition);
       servoPosition += direction;
-      delay(delay);
+      ::delay(delay);
+    }
   }
 
   void perform (int to) {
-    perform(to, DelayPerGrad)
+    perform(to, degDelay);
   }
-}
 
-rot3u_6dof_servo servo;
+  //__declspec(property(get = getDegDelay, put = setDegDelay)) int DelayPerGrad; 
+  // __declspec(property(get = getPortNumber, put = Initialize)) int Port; 
+  
+};
+
+const int DPORT1 = 1;
+
+SGServo servo;
 
 // Set initial position
 void reset() {
@@ -71,7 +80,7 @@ void reset() {
 }
 
 void setup() {
-  rot3u_6dof_servo.Initialize(DPORT1);
+  servo.initialize(DPORT1);
   delay(500);
   reset();
   // подождем 1с пока все приводы закончат позиционирование
