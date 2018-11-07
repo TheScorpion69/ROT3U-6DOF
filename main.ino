1c75f3e53d9263d558ef33a8a254c32ec854398a
igorgrebenev <igor.ge@mail.ru>
2018-11-07 21:23:54 +0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -3,44 +3,59 @@
 #import "ROT3U6DOF.h"
 
 //Arduino UNO DIGITAL PORTS 0 .. 13
+/*
 #define DP1 1
+#define DP2 2
+#define DP3 3
+#define DP4 4
+#define DP5 5
+#define DP6 6
 
-SGServo servo;
-
+SGServo servo1;
+SGServo servo2;
+SGServo servo3;
+SGServo servo4;
+SGServo servo5;
+SGServo servo6;
+*/
 void setup() {
-  // setupROT3U6DOF(0, 0); // Initialize servos ROT3U6DOF_SERVO_COUNT/ROT3U6DOF_SERVO_START_PORT
-  // delay(2000); // подождем пока все приводы проинициализируются
-  // resetROT3U6DOF(); // установим кутяплю в исходное положение
+  setupROT3U6DOF(0, 0); // 500-900Initialize servos ROT3U6DOF_SERVO_COUNT/ROT3U6DOF_SERVO_START_PORT
+  resetROT3U6DOF(); // установим кутяплю в исходное положение
+  delay(2000); // подождем 2 сек
+  
+  /*
+  servo1.initialize(DP1);
+  servo2.initialize(DP2);
+  servo3.initialize(DP3);
+  servo4.initialize(DP4);
+  servo5.initialize(DP5);
+  servo6.initialize(DP6);
 
-  //****************************************
-  servo.initialize(DP1);
-  // https://arduinomaster.ru/motor-dvigatel-privod/servoprivody-arduino-sg90-mg995-shema-podklyuchenie-upravlenie/
-  // servo.setTimes(544, 2400)
-  delay(500);
-  servo.perform(90);
-  // подождем 1с пока все приводы закончат позиционирование
-  delay(1000);
-  //****************************************
+  servo1.performImmediately(45);
+  servo2.performImmediately(120);
+  servo3.performImmediately(120);
+  servo4.performImmediately(90);
+  servo5.performImmediately(180);
+  servo6.performImmediately(20);
+  */
+
 }
 
 void loop() {
-  /*
-  // TODO
-  int position_step_01[] = {
-    0, // Servo 1 position
-    10, // Servo 2 position
-    12, // Servo 3 position
-    UNDEFINED, // Servo 4 position
-    UNDEFINED, // Servo 5 position
-    UNDEFINED // Servo 6 position
-  };
-  performAllServos(position_step_01);
-  */
-  //****************************************
-  // тест для колиброви сервопривода
-  servo.perform(0);
-  delay(5000);
-  servo.perform(180);
-  delay(5000);
-  //****************************************
+  int positions1[] = {UNDEFINED, UNDEFINED, UNDEFINED, UNDEFINED, UNDEFINED, 60};
+  performAllServos(positions1);
+  delay(3000);
+  
+  int positions2[] = {UNDEFINED, UNDEFINED, 22, 120, UNDEFINED, UNDEFINED};
+  performAllServos(positions2);
+  delay(3000);
+ 
+  int positions3[] = {90, UNDEFINED, UNDEFINED, UNDEFINED, UNDEFINED, UNDEFINED};
+  performAllServos(positions3);
+  delay(3000);
+  
+  int positions4[] = {UNDEFINED, UNDEFINED, 52, 160, UNDEFINED, UNDEFINED};
+  performAllServos(positions4);
+  delay(30000);
+  
 }
